0211b607b5fdd75a016e5926121cdf5c51d4c61b
Kent Oshiro <oshiro@uw.edu>
2015-06-25 11:12:14 -0700

    made public folder
    and stuff like that

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-﻿var http = require('http');
+﻿var express = require('express');
+var app = express();
+// var mongoose = require('mongoose');
+
+var http = require('http');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
+
+console.log("WEEEEEEEEEEEEE");
