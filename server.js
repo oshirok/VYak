61f81df2e8da3610fac8cf00848d38cfb4f9d99c
Kent Oshiro <oshiro@uw.edu>
2015-06-25 12:10:41 -0700

    api works

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 ﻿var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
 var http = require('http');
 var port = process.env.port || 1337;
+
+var date = new Date();
+
+mongoose.connect('mongodb://vyak:vyak@ds047762.mongolab.com:47762/vyak');
+
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
 // Models
 var Message = mongoose.model('Message', {
@@ -11,12 +22,13 @@ var Message = mongoose.model('Message', {
     text : String,
 });
 
-mongoose.connect('mongodb://vyak:vyak@ds047762.mongolab.com:47762/vyak');
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
 
 // Making RESful API routes
 app.get('/api/messages', function (req, res) {
     
-    Messages.find(function (err, messages) {
+    Message.find(function (err, messages) {
         if (err)
             res.send(err);
         res.json(messages); // return all messages in JSON format
@@ -25,8 +37,9 @@ app.get('/api/messages', function (req, res) {
 });
 
 app.post('/api/messages', function (req, res) {
-    Messages.create({
-        timestamp: req.body.timestamp,
+    console.log(req.body);
+    Message.create({
+        timestamp: date.getTime(),
         text: req.body.text,
         done: false
     }, function (err, messages) {
@@ -34,15 +47,12 @@ app.post('/api/messages', function (req, res) {
             res.send(err);
         
         // return all messages after you create another
-        Messages.find(function (err, messages) {
+        Message.find(function (err, messages) {
             if (err)
                 res.send(err)
             res.json(messages);
         });
     });
 });
-
-app.use(express.static(__dirname + '/public'));
-app.listen(port);
 
 console.log("WEEEEEEEEEEEEE" + port);
