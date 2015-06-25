cc209f055c4c8734e97b484d915923fb17802f13
Michael Chun <chunm404@gmail.com>
2015-06-25 16:16:19 -0700

    this is likely going to break

--- public/core.js
+++ public/core.js
@@ -1,8 +1,36 @@
 // public/core.js
 var vertaYak = angular.module('vertaYak', []);
 
-function mainController($scope, $http) {
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
+
+function mainController($scope, $http, socket) {
     $scope.formData = {};
+	
+	socket.on('please_update_now', function (){
+		console.log("reached socket.");
+	});
 
     // get all messages and show them
     $http.get('/api/messages')
